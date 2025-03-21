80c4bc426f1734d455e3201f8298d5ffae6fb396
xbru1 <xbrumley.edu@gmail.com>
2025-03-20 21:12:30 -0400

    Pylint

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+"""Server for Emotion Detector application"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,19 +7,20 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
+    """Render the main page"""
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def sent_detector():
-
+    """Get user input and return a response"""
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
 
-    if (response['dominant_emotion'] == 'none'):
+    if response['dominant_emotion'] == 'none':
         return "Invalid Text! Please Try Again"
 
-    toReturn = f'''
+    to_return = f'''
     For the given statement, the system response is
     \n'anger': {response['anger']}
     \n'disgust': {response['disgust']}
@@ -27,7 +30,7 @@ def sent_detector():
     \nThe dominant emotion is {response['dominant_emotion']}
     '''
 
-    return toReturn
+    return to_return
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000)
